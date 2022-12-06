bf23095d492be48cd3734957b99e7933602b4a20
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-12-06 09:30:08 +0100

    some ideas

--- src/5/index.js
+++ src/5/index.js
@@ -5,5 +5,14 @@ let file = fs.readFileSync(path.join(`${__dirname}/input.txt`), {encoding: 'utf8
 let operations = file.split("\n").filter(f => f.startsWith("move"));
 
 // Filter everything expect creates
-let crates = file.split("\n").filter(f => !f.startsWith("move") && f != "").slice(0,-1).map(line => line.split( " "))
+let crates = file.split("\n").filter(f => !f.startsWith("move") && f != "").slice(0,-1).map(line => line.split( "    "))
 
+let createsMap = new Map()
+for (let i = 0; i < crates.length; i++) {
+    for (let j = 0; j < crates[i].length; j++) {
+        let crateElement = crates[i][j];
+        createsMap.set(i, [createsMap.get(i), crateElement])
+    }
+}
+
+console.log(createsMap)
