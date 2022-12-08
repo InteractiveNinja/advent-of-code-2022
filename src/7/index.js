4a8aaa8a7345f66372dcdb66cc460333e7bcf5c8
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-12-08 09:35:57 +0100

    some more ideas, recursive stuff

--- src/7/index.js
+++ src/7/index.js
@@ -4,53 +4,86 @@ let file = fs.readFileSync(path.join(`${__dirname}/input.txt`), {encoding: 'utf8
 
 const lines = file.split("\n")
 
-const folderMap = new Map()
-lines.reduce((folder, currentValue) => {
 
-    if(folder != "" && !folderMap.get(folder)) folderMap.set(folder, [])
-    const line = currentValue;
-    let currentFolder = folder;
+const filesAndFoldersMap = new Map()
 
-    // add Folders
-    if(line.includes("$") && line.includes("cd") && !line.includes("..")) {
-        currentFolder = line.split(" ").slice(-1).pop();
-    }
+lines.reduce((folderName, currentLine) => {
+    let currentFolderName = folderName;
 
-    if(!line.includes("$")) {
-        let value;
-        if(!line.includes("dir")) {
-            // Filesize
-            value = line.split(" ").slice(0, 1)
-        } else {
-            // Foldername
-            value = line.split(" ").slice(1)
+    if (currentLine.startsWith("$")) {
+        // CMD Line
+
+        if (currentLine.includes("cd") && !currentLine.includes("..")) {
+            currentFolderName = currentLine.split(" ").pop()
         }
-        folderMap.set(folder, [...folderMap.get(folder), ...value])
+
+    } else {
+        let fileOrFolder;
+        if (currentLine.includes("dir")) {
+            // is Folder
+            fileOrFolder = currentLine.split(" ").pop();
+        } else {
+            fileOrFolder = currentLine.split(" ")[0];
+        }
+        filesAndFoldersMap.set(currentFolderName, [...filesAndFoldersMap.get(currentFolderName), fileOrFolder])
+
+    }
+    if (currentFolderName !== "") {
+        if (!filesAndFoldersMap.get(currentFolderName)) filesAndFoldersMap.set(currentFolderName, [])
     }
 
+    return currentFolderName;
 
-    return currentFolder;
 }, "")
 
-const folderSum = new Map()
+// Calculates sum from Folder where only Folders exists
+let folderSum = new Map(Array.from(filesAndFoldersMap.entries()).map((mapEntry) => {
+    const [folder, fileAndFolder] = mapEntry;
 
-Array.from(folderMap.entries()).forEach(([key,value]) => {
-    let sum = value.reduce((prev, currentValue) => prev += isNaN(currentValue) ? 0 : parseInt(currentValue), 0);
+    let onlyFiles = fileAndFolder.every(entry => !isNaN(entry));
 
-    folderSum.set(key, sum)
+    if (onlyFiles) {
+        let sum = fileAndFolder.reduce((prev, cur) => prev + parseInt(cur), 0);
+        return [folder, sum]
+    } else {
+
+        return mapEntry;
+    }
+
+}));
+
+function getFolderSizeRec(folderName) {
+    let folderValueOrSize = folderSum.get(folderName);
+    if(!folderValueOrSize) {
+        return parseInt(folderName)
+    }
+    if (isNaN(folderValueOrSize)) {
+
+        return folderValueOrSize.reduce((prev, cur) => prev + isNaN(cur) ? parseInt(getFolderSizeRec(cur)) : parseInt(cur), 0)
+
+    }
+    return parseInt(folderValueOrSize)
+}
+
+
+const totalSum = Array.from(folderSum.entries()).map(mapEntry => {
+    const [folder, fileAndFolder] = mapEntry;
+    if (!isNaN(fileAndFolder)) {
+        return mapEntry;
+    }
+
+    let fileSum = fileAndFolder.filter(file => !isNaN(file)).reduce((prev, cur) => prev + parseInt(cur), 0);
+    let folders = fileAndFolder.filter(folder => isNaN(folder));
+    return [folder, [fileSum, folders]]
+}).map(mapEntry => {
+    if (!isNaN(mapEntry[1])) {
+        return mapEntry;
+    }
+    const [folder, [fileSize, folders]] = mapEntry;
+
+    const folderSum = folders.reduce((prev, cur) => prev + getFolderSizeRec(cur), 0)
+    return [folder, fileSize + folderSum]
+
 })
+console.log(totalSum)
 
-console.log(folderSum)
-
-const totalSum = new Map()
-
-
-Array.from(folderMap.entries()).forEach(([key,value]) => {
-    let innerFolders = value.filter(val => isNaN(val));
-    let innerFolderSum = innerFolders.reduce((prev, current) => prev += folderSum.get(current), 0);
-    totalSum.set(key, folderSum.get(key) + innerFolderSum)
-
-});
-
-let sum = Array.from(totalSum.values()).filter(value => value <= 100000).reduce((previousValue, currentValue) => previousValue + currentValue, 0);
-console.log(sum)
